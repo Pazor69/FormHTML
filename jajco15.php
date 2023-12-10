7c300b931dae184cef7ddd4d407d31f5b58f7a51
Pazor69 <71083834+Pazor69@users.noreply.github.com>
2023-12-10 18:26:40 +0100

    Update jajco15.php

--- jajco15.php
+++ jajco15.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Twoja Strona</title>
+    <title>PMS Globale</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
